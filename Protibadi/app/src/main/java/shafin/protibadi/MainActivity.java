e5be4fd7e3eabce5f5231e6d317a4875869f942e
shafin <oshafin@gmail.com>
2017-12-14 01:35:34 +0600

    call added

--- Protibadi/app/src/main/java/shafin/protibadi/MainActivity.java
+++ Protibadi/app/src/main/java/shafin/protibadi/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends SettingActivity implements LocationListener {
 
     final int SEND_SMS_PERMISSION_REQUEST_CODE = 111;
     private  Button mSendMessageBtn;
-    Button getLocationBtn,setting;
+    Button getLocationBtn,setting,call;
     TextView locationText;
     double l;
     double n;
@@ -111,8 +111,38 @@ public class MainActivity extends SettingActivity implements LocationListener {
             }
         });
 
+        call=(Button)findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                call();
+            }
+        });
 
 
+    }
+
+    private void call() {
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL); //use ACTION_CALL class
+        callIntent.setData(Uri.parse("tel:"+getPhone()));    //this is the phone number calling
+        //check permission
+        //If the device is running Android 6.0 (API level 23) and the app's targetSdkVersion is 23 or higher,
+        //the system asks the user to grant approval.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            //request permission from user if the app hasn't got the required permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},   //request specific permission from user
+                    10);
+            return;
+        }else {     //have got permission
+            try{
+                startActivity(callIntent);  //call activity and make phone call
+            }
+            catch (android.content.ActivityNotFoundException ex){
+                Toast.makeText(getApplicationContext(),"yourActivity is not founded",Toast.LENGTH_SHORT).show();
+            }
+        }
 
     }
 
